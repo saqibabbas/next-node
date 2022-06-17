3abee35f302fcf30475169aed42b52dcac1cb883
Saqib Abbas <saqib4cs@gmail.com>
2022-06-17 19:46:43 +0500

    connection updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-let mongoUrl='mongodb://127.0.0.1:27017';
+let mongoUrl='mongodb://127.0.0.1:27017/SMart';
 if(process.env.MONGODB_URI){
     mongoUrl=process.env.MONGODB_URI;
 }
-mongoose.connect(`${mongoUrl}/SMart`).then(() => {
+mongoose.connect(`${mongoUrl}`).then(() => {
     // port will be configurable later
     const port = process.env.PORT || 8080;
     server = app.listen(port, () => {
