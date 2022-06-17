de6da4037f8b2bb5252b6926b299aa833d90c52f
Saqib Abbas <saqib4cs@gmail.com>
2022-06-17 15:00:16 +0500

    port updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 // port will be configurable later
-const port = 3000;
+const port = process.env.PORT || 8080;
 server = app.listen(port, () => {
     console.log(`Listening to port ${port}`);
 });
