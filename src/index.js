ca3d02222791a8489f3c2b4a200ecb69e3f46d53
Saqib Abbas <saqib4cs@gmail.com>
2022-06-17 18:30:25 +0500

    MONGODB_URI added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/SMart').then(() => {
+const mongoUrl='mongodb://127.0.0.1:27017';
+if(process.env.MONGODB_URI){
+    mongoUrl=process.env.MONGODB_URI;
+}
+mongoose.connect(`${mongoUrl}/SMart`).then(() => {
     // port will be configurable later
     const port = process.env.PORT || 8080;
     server = app.listen(port, () => {
