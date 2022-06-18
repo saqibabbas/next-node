a23d064453697fb6f3a50b38eb4a1bd4e317d05f
Saqib Abbas <saqib4cs@gmail.com>
2022-06-18 14:46:55 +0500

    nodemon and dotenv implementation added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 const mongoose = require('mongoose');
+require("dotenv").config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/SMart').then(() => {
     // port will be configurable later
