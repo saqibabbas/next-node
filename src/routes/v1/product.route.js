64fb744d675ab73c4ab437b11d8fa6a346171791
Saqib Abbas <saqib4cs@gmail.com>
2022-06-19 23:00:01 +0500

    auth layer added on product routes

--- src/routes/v1/product.route.js
+++ src/routes/v1/product.route.js
@@ -1,12 +1,12 @@
 const express =  require('express');
 const { productValidation } = require('../../validations');
 const { productController } = require('../../controllers');
-const { validate } = require('../../middlewares');
+const { validate, auth } = require('../../middlewares');
 const router = express.Router();
 
 router
     .route('/')
-    .get(productController.getProducts)
-    .post(validate(productValidation.saveProduct),productController.saveProduct)
+    .get(auth, productController.getProducts)
+    .post(auth, validate(productValidation.saveProduct),productController.saveProduct)
 
 module.exports = router;
